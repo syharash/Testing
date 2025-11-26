e5e047cb00f9a9b9ea610fa5df3ebc11267cefd1
syharash <138817364+syharash@users.noreply.github.com>
2025-11-26 10:50:37 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,14 +5,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// --- Firebase Initialization ---
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB4kSCgdJLSCFMUdEyqT_I9xgrEFNnPulQ",
+  authDomain: "ijtima-ae364.firebaseapp.com",
+  projectId: "ijtima-ae364",
+  storageBucket: "ijtima-ae364.firebasestorage.app",
+  messagingSenderId: "843664116262",
+  appId: "1:843664116262:web:b6144381ce4b92934374e7",
+  measurementId: "G-HGZZGTGFZN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 };
 
 const app = initializeApp(firebaseConfig);
